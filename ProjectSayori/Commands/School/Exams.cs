d6627c38af756b3cba67ce6dbb360a0ef9f809b7
arisatominato48@gmail.com <arisatominato48@gmail.com>
2021-11-26 13:06:44 +0800

    Updated School Things

---
+++ ProjectSayori/Commands/School/Exams.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Newtonsoft.Json.Linq;
+using DSharpPlus.Entities;
+
+namespace ProjectSayori.Commands
+{
+    public class Exams : BaseCommandModule
+    {
+
+        string[] subjects =
+        {
+            /*0*/ "Computer Programming 1",
+            /*1*/ "Physical Fitness and Self-Testing Activities",
+            /*2*/ "Filipinolohiya at Pambansang Kaunlaran",
+            /*3*/ "Purposive Communication",
+            /*4*/ "Politics, Governance and Citizenship",
+            /*5*/ "Introduction to Computing",
+            /*6*/ "Mathematics in the Modern World",
+            /*7*/ "Civic Welfare Training Service 1"
+        };
+
+        [Command("exam")]
+        [Description("Exams? Lol, are you reviewing?")]
+        public async Task Exam(CommandContext ctx)
+        {
+            DateTime now = DateTime.Now;
+            var builder = new DiscordEmbedBuilder
+            {
+                Title = "Exams",
+                Color = DiscordColor.Red,
+                Description = $"Here is the schedule of the exams as of 11/23."
+            };
+
+            builder.AddField(subjects[0], "December 9(?)");
+            builder.AddField(subjects[5], "December 14(?)");
+            builder.AddField(subjects[6], "Probably Next Week or Next Two Weeks.");
+
+            var embed = await ctx.Channel.SendMessageAsync(embed: builder).ConfigureAwait(false);
+        }
+    }
+}
