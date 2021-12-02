f4692ed4f8b9a5f3db3c35d2ba7bb5ba76922c93
arisatominato48@gmail.com <arisatominato48@gmail.com>
2021-12-02 15:25:50 +0800

    Updated Assignments as of 12/01

--- ProjectSayori/Commands/School/Assignments.cs
+++ ProjectSayori/Commands/School/Assignments.cs
@@ -35,14 +35,12 @@ namespace ProjectSayori.Commands
             {
                 Title = "Assignments",
                 Color = DiscordColor.Red,
-                Description = $"Last Updated: 11/16/2021"
+                Description = $"Last Updated: 12/01/2021"
             };
 
             builder.AddField(subjects[2], "> Basahin ang modyul at pumili ng dalawang gawain na nais mong sagutan\n> mula sa Gawan 2, 3, at 6 \n" +
                 "> Ang Modyul: https://drive.google.com/file/d/1dtm0zohDOGAKttr_6aDbXDAonkF29fst/view?usp=sharing" +
                 "\n*1?/??/2021 (Bago mag bakasyon)*");
-            builder.AddField(subjects[4], "> Make a 250-word think-piece/critique of Gloc-9's ''Upuan''. The piece should identify the gaps and contexts of the present day socio-political realities in the country and the power that they need to exercise." +
-                "\n*11/12/21*");
             builder.AddField(subjects[7], "> Answer Module 1 Assessment in Google Classroom" +
                 "\n*11/??/21*");
             builder.AddField(subjects[6], "> Quiz! From Patterns to Propositions" +
