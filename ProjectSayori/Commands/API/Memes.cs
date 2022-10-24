af0190dca8fa954d681f06ad542faa90c18c01f0
arisatominato48@gmail.com <arisatominato48@gmail.com>
2022-10-24 15:06:13 +0800

    Add "r/Animemes"

--- ProjectSayori/Commands/API/Memes.cs
+++ ProjectSayori/Commands/API/Memes.cs
@@ -32,5 +32,25 @@ namespace ProjectSayori.Commands
             };
             var embed = await ctx.Channel.SendMessageAsync(embed: builder).ConfigureAwait(false);
         }
+
+        [Command("animemes")]
+        [Description("Picks a random meme from r/animemes.")]
+        public async Task MemesAnime(CommandContext ctx)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("https://reddit.com/r/animemes/random.json?limit=1");
+            JArray arr = JArray.Parse(result);
+            JObject post = JObject.Parse(arr[0]["data"]["children"][0]["data"].ToString());
+
+
+            var builder = new DiscordEmbedBuilder
+            {
+                Title = post["title"].ToString(),
+                Color = DiscordColor.Green,
+                ImageUrl = post["url"].ToString(),
+                Url = "https://reddit.com" + post["permalink"].ToString()
+            };
+            var embed = await ctx.Channel.SendMessageAsync(embed: builder).ConfigureAwait(false);
+        }
     }
 }
